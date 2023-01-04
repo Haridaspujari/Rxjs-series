38641bf5aff2df573958e4913b4e8ecb64922024
Haridas <haridaspujari123@gmail.com>
2023-01-04 19:39:13 +0530

    rxjs Practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Rxjs-Series';
+export class AppComponent implements AfterViewInit, OnInit {
+
+  @ViewChild("startBtn") startBtn: ElementRef | any;
+
+
+  constructor() { 
+    let result2 : any= document.getElementById("result2")
+    result2.innerHTML='Hari'
+  }
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    let result1 = document.getElementById("result1")
+    fromEvent(this.startBtn.nativeElement, 'click').subscribe(res => {
+
+    })
+
+  }
+
+  print(){}
 }
